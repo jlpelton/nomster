033e6c039b8faefa7be2a358a726e49572bcbe15
jlpelton <jennifer.pelton@hmhco.com>
2015-11-06 19:00:53 +0000

    carrierwaveuploader

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,3 +7,17 @@ end
 class Photo < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
 end
+
+
+u = Photo.new
+u.picture = params[:file] # Assign a file like this, or
+
+# like this
+File.open('somewhere') do |f|
+  u.picture = f
+end
+
+u.save!
+u.picture.url # => '/url/to/file.png'
+u.picture.current_path # => 'path/to/file.png'
+u.picture_identifier # => 'file.png'
