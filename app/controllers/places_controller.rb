dce4a330a7427857ec506b8d413f52c55212b093
jlpelton <jennifer.pelton@hmhco.com>
2015-11-10 21:01:31 +0000

    added ice cream cone photo

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,7 +16,7 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find(params[:id])
     @comment = Comment.new
-    @photo = Photo.new
+    @photo = Photo.new 
   end
 
  def edit
